12928ec65064339ba084debc0701e8c1e458409c
LArryTest21 <larryscape21@gmail.com>
2023-10-26 17:43:32 +0200

    dsa

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -2,21 +2,14 @@ import express, { Router } from "express";
 import serverless from "serverless-http";
 import ViteExpress from 'vite-express'
 import AuthRoutes from "../../src/server/routes/authRoutes"
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-import bodyParser from "body-parser"
 
-const mongoURI = process.env.MONGO_URI;
-const database = mongoose.connection;
+const api = express();
 
-const app = express();
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
-mongoose.connect(mongoURI)
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(cookieParser())
-app.use('/api', AuthRoutes)
+api.use("/api", router);
 
-export const handler = serverless(app);
+export const handler = serverless(api);
 
