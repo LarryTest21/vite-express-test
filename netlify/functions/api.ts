7e924ea08f3df7d0f854d9ffd746961cabc100c5
LArryTest21 <larryscape21@gmail.com>
2023-10-27 13:15:38 +0200

    sdf

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -18,4 +18,67 @@ ViteExpress.listen(api, 5173, () =>
 
 api.use("/api", router);
 
-export const handler = serverless(api);
+
+
+
+
+
+import 'dotenv/config'
+import authRoutes from "../../src/server/routes/authRoutes"
+import bodyParser from "body-parser"
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
+
+const app = express();
+
+
+// database connection
+
+const mongoURI = process.env.MONGO_URI;
+const database = mongoose.connection;
+database.on('error', (error: any) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+ViteExpress.config({ mode: "production" })
+
+ViteExpress.listen(app, 5173, () =>
+    console.log("Server is listening on http://localhost:5173"))
+
+mongoose.connect(mongoURI)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(cookieParser())
+app.use('/api', authRoutes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const handler = serverless(app);
