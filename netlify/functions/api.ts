bd7295b5b61a0211079dea09be9d705d42e81398
LArryTest21 <larryscape21@gmail.com>
2023-10-27 10:04:05 +0200

    jkl

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,12 +1,35 @@
 
-import express, { Router } from "express";
 import serverless from "serverless-http";
+import express from "express"
+import 'dotenv/config'
+import ViteExpress from "vite-express";
+import authRoutes from "../../src/server/routes/authRoutes"
+import bodyParser from "body-parser"
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
 
-const api = express();
+const app = express();
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+// database connection
 
-export const handler = serverless(api);
+const mongoURI = process.env.MONGO_URI;
+const database = mongoose.connection;
+database.on('error', (error: any) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+ViteExpress.config({ mode: "production" })
+
+mongoose.connect(mongoURI)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(cookieParser())
+app.use('/api', authRoutes)
+
+export const handler = serverless(app);
