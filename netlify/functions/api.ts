05f3bee22b283a61e29ab0b08b066fa94ecffaac
LArryTest21 <larryscape21@gmail.com>
2023-10-27 13:25:02 +0200

    dfg

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -2,24 +2,7 @@ import serverless from "serverless-http";
 import express, { Router } from "express";
 import ViteExpress from "vite-express";
 
-const api = express();
-
-
-const router = Router();
-
-
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-
 ViteExpress.config({ mode: "production" })
-
-ViteExpress.listen(api, 5173, () =>
-    console.log("Server is listening on http://localhost:5173"))
-
-api.use("/api", router);
-
-
-
 
 
 
@@ -31,8 +14,9 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 
+ViteExpress.listen(app, 5173, () =>
+    console.log("Server is listening on http://localhost:5173"))
 
-// database connection
 
 const mongoURI = process.env.MONGO_URI;
 const database = mongoose.connection;
@@ -55,30 +39,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(cookieParser())
 app.use('/api', authRoutes)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 export const handler = serverless(app);
