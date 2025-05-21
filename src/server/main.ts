521ec5307ff7721514c80d9a302344bedb102277
LarryTest21 <122280302+LarryTest21@users.noreply.github.com>
2025-05-22 00:05:24 +0200

    new

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,33 +1,38 @@
 import serverless from "serverless-http";
 import express, { Router } from "express";
 import ViteExpress from "vite-express";
-import 'dotenv/config'
-import authRoutes from "../../src/server/routes/appRoutes"
-import bodyParser from "body-parser"
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
+import "dotenv/config";
+import authRoutes from "../../src/server/routes/appRoutes";
+import bodyParser from "body-parser";
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 const mongoURI = process.env.MONGO_URI;
 const database = mongoose.connection;
-database.on('error', (error: any) => {
-    console.log(error)
-})
+database.on("error", (error: any) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
-
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI);
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json({ limit: '100mb' }));
-app.use(cookieParser())
+app.use(bodyParser.json()); 
 
-
-app.use('/api/*', authRoutes)
-
+app.use("/api/*", authRoutes);
 
 export const handler = serverless(app);
+
+process.on("exit", () => {
+  console.log("Server shutting down...");
+  process.exit();
+});
+process.on("SIGINT", () => {
+  console.log("Caught interrupt signal, stopping server...");
+  process.exit();
+});
