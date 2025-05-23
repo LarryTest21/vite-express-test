3f745f63a8543f6f559a0a9cf7c93618f74e3d6c
LarryTest21 <122280302+LarryTest21@users.noreply.github.com>
2025-05-23 11:47:31 +0200

    asd

--- functions/api.ts
+++ functions/api.ts
@@ -4,14 +4,16 @@ import ViteExpress from "vite-express";
 import 'dotenv/config';
 import appRoutes from "../src/server/routes/appRoutes";
 import chatRouter from "../src/server/chatControllers/mainChat";
-
 import bodyParser from "body-parser";
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+
 const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI ?? "";
+
 const database = mongoose.connection;
 database.on('error', (error: any) => {
   console.log(error);
@@ -21,6 +23,7 @@ database.once('connected', () => {
   console.log('Database Connected');
 });
 mongoose.connect(mongoURI);
+
 
 const viteServer = ViteExpress.listen(app, 3333, () => {
   console.log("Server started");
