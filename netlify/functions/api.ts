606253fc763497b6cf2f9ac61fe330191e7c5e23
LarryTest21 <122280302+LarryTest21@users.noreply.github.com>
2025-05-24 10:39:23 +0200

    asd

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,23 +1,24 @@
 import serverless from "serverless-http";
 import express, { Router } from "express";
-import ViteExpress from "vite-express";
 import 'dotenv/config'
 import authRoutes from "../../src/server/routes/appRoutes"
 import bodyParser from "body-parser"
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
+
 const cookieParser = require('cookie-parser')
 
 const app = express();
 
 const mongoURI = process.env.MONGO_URI;
-const database = mongoose.connection;
-database.on('error', (error: any) => {
-    console.log(error)
-})
+if (!mongoURI) {
+  throw new Error('Missing MONGO_URI in environment variables');
+}
 
+const database = mongoose.connection;
+database.on('error', console.error);
 database.once('connected', () => {
     console.log('Database Connected');
-})
+});
 
 
 mongoose.connect(mongoURI)
@@ -27,7 +28,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(cookieParser())
 
 
-app.use('/api', authRoutes)
+app.use('/', authRoutes)
 
 
 export const handler = serverless(app);
