63b61a6abd295a4007f032775c0218483298ff56
LarryTest21 <122280302+LarryTest21@users.noreply.github.com>
2025-05-24 11:16:17 +0200

    ds

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,45 +1,61 @@
-import serverless from "serverless-http";
-import express, { Router } from "express";
-import 'dotenv/config'
-import authRoutes from "../../src/server/routes/appRoutes"
-import bodyParser from "body-parser"
-import mongoose from "mongoose";
+// import serverless from "serverless-http";
+// import express, { Router } from "express";
+// import 'dotenv/config'
+// import authRoutes from "../../src/server/routes/appRoutes"
+// import bodyParser from "body-parser"
+// import mongoose from "mongoose";
 
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
+
+// const app = express();
+
+// const mongoURI = process.env.MONGO_URI;
+// if (!mongoURI) {
+//   throw new Error('Missing MONGO_URI in environment variables');
+// }
+
+// const database = mongoose.connection;
+// database.on('error', console.error);
+// database.once('connected', () => {
+//     console.log('Database Connected');
+// });
+
+
+// mongoose.connect(mongoURI)
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(bodyParser.json({ limit: '10mb' }));
+// app.use(cookieParser())
+
+
+// app.use('/', authRoutes)
+// app.get('/test', (_, res) => {
+//   res.json({ message: 'Function is working!' });
+// });
+// app.get('/', (req, res) => {
+//   res.send('API root is working!');
+// });
+
+
+// export const handler = async () => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: 'Hello from Netlify!' }),
+//   };
+// };
+
+
+import serverless from "serverless-http";
+import express from "express";
 
 const app = express();
 
-const mongoURI = process.env.MONGO_URI;
-if (!mongoURI) {
-  throw new Error('Missing MONGO_URI in environment variables');
-}
-
-const database = mongoose.connection;
-database.on('error', console.error);
-database.once('connected', () => {
-    console.log('Database Connected');
-});
-
-
-mongoose.connect(mongoURI)
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(cookieParser())
-
-
-app.use('/', authRoutes)
-app.get('/test', (_, res) => {
-  res.json({ message: 'Function is working!' });
-});
 app.get('/', (req, res) => {
   res.send('API root is working!');
 });
 
+app.get('/test', (req, res) => {
+  res.json({ message: "Test route works!" });
+});
 
-export const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Netlify!' }),
-  };
-};
+export const handler = serverless(app);
