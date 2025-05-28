f5ce3ee7608a9bb7fd0e62366d879b70e0f6aa4d
LarryTest21 <122280302+LarryTest21@users.noreply.github.com>
2025-05-28 10:24:10 +0200

    sd

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-
-
-  resolve: {
-    alias: {
-      process: "process/browser",
-      zlib: "browserify-zlib",
-      util: "util",
-    },
-  },
   plugins: [vue()],
-    server: {
-    port: 5173
+  server: {
+    port: 5173,
   },
-
 });
