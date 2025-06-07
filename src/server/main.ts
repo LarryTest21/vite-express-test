a99ed7d2215a079e1c63c292699e4fc89e6ce2d3
LarryTest21 <larryscape21@gmail.com>
2025-06-07 11:12:22 +0200

    fs

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,6 +1,5 @@
 import serverless from "serverless-http";
 import express, { Router } from "express";
-import ViteExpress from "vite-express";
 import 'dotenv/config'
 import authRoutes from "../../src/server/routes/appRoutes"
 import bodyParser from "body-parser"
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import nodePolyfills from "@emreerdogan/vite-plugin-node-stdlib-browser";
 
 export default defineConfig({
-  plugins: [vue(), nodePolyfills()],
+  plugins: [vue()],
 
   server: {
+   
     port: 5173,
   },
 });
