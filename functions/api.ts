860aa04e485ae40ada9f091fe20a85ebc07e8522
LarryTest21 <larryscape21@gmail.com>
2025-06-12 22:30:20 +0200

    asd

--- functions/api.ts
+++ functions/api.ts
@@ -42,6 +42,5 @@ app.use(async (req, res, next) => {
 
 router.get("/test", (_, res) => { res.json({ message: "✅ Express function works!" }); });
 
-app.use('/.netlify/functions/api', router);
-router.use("/api", appRoutes);
+app.use('/.netlify/functions/api', appRoutes);
 export const handler = serverless(app);
