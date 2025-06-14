222390adea80ca0b578875a287f9418a10a71fe8
LarryTest21 <larryscape21@gmail.com>
2025-06-14 09:44:53 +0200

    asd

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -38,6 +38,10 @@ app.use((req, res, next) => {
   console.log("🔎 Incoming:", req.method, req.url);
   next();
 });
+router.use((req, res, next) => {
+  console.log("🧭 Matched route:", req.method, req.url);
+  next();
+});
 
 // Routes
 router.get("/test", (_, res) => {
